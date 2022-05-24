78d587f9abbd1460d40eaded24068a2b9a702fac
eXiga <anton.kostenich@hotmail.com>
2022-05-25 01:22:58 +0200

    Add ability to fetch pokemon by name

--- src/data/fetcher.ts
+++ src/data/fetcher.ts
@@ -1,7 +1,7 @@
 import { Pokemon } from "./types/pokemon";
 
-export async function fetchPokemon(id: number): Promise<Pokemon> {
-  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+export async function fetchPokemon(fetchBy: number | string): Promise<Pokemon> {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${fetchBy}`);
   const json = await response.json();
   return new Pokemon(json);
 }
