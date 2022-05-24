decdcd0294a5b5c9305adef36aaf3aa7498c9516
eXiga <anton.kostenich@hotmail.com>
2022-05-25 01:26:07 +0200

    Add event handlers and basic error handling to SearchBar component

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,26 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function SearchBar() {
+interface SearchBarProps {
+  hasError: boolean;
+  onRandomButtonClicked: () => void;
+  onSearchButtonClicked: (name: string) => void;
+}
+
+export default function SearchBar({
+  hasError,
+  onRandomButtonClicked,
+  onSearchButtonClicked,
+}: SearchBarProps) {
+  const [name, setName] = useState("");
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
+
   return (
     <div className="flex flex-col justify-center items-center h-screen gap-y-5">
       <div>
         <input
-          className="shadow w-96 appearance-none border rounded py-2 px-2 text-sky-600 leading-tight italic placeholder:italic"
+          className={`w-96 appearance-none border-2 border-solid rounded py-2 px-2 ${
+            hasError ? "border-red-700 outline-red-700" : ""
+          } text-sky-600 leading-tight italic placeholder:italic`}
           id="name"
           type="text"
           placeholder="Pokemon name"
+          value={name}
+          onChange={handleInputChange}
+          onKeyDown={(event: React.KeyboardEvent<HTMLInputElement>) => {
+            if (event.key === "Enter") {
+              setName(event.currentTarget.value);
+              onSearchButtonClicked(name);
+            }
+          }}
         />
       </div>
       <div className="flex flex-row justify-start items-start gap-x-2">
         <button
           className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
           type="button"
+          onClick={() => onSearchButtonClicked(name)}
         >
           Search
         </button>
         <button
           className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
           type="button"
+          onClick={onRandomButtonClicked}
         >
           I am lucky!
         </button>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,18 +5,31 @@ import { Pokemon } from "../data/types/pokemon";
 
 export default function Home() {
   const [pokemon, setPokemon] = useState<Pokemon | undefined>(undefined);
+  const [hasError, setHasError] = useState(false);
 
-  // This code will be commented for a while, I'm focused on layout now lol
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const pokemon = await fetchPokemon(Math.floor(Math.random() * 100));
-  //     setPokemon(pokemon);
-  //   };
+  const randomPokemon = async () => {
+    const pokemon = await fetchPokemon(Math.floor(Math.random() * 100));
+    console.log(pokemon);
+  };
 
-  //   fetchData();
-  // }, []);
+  const searchPokemon = async (name: string) => {
+    try {
+      const pokemon = await fetchPokemon(name);
+      setHasError(false);
+      console.log(pokemon);
+    } catch (error) {
+      if (error) {
+        setHasError(true);
+        return;
+      }
+    }
+  };
 
-  // if (!pokemon) return <div>Loading...</div>;
-
-  return <SearchBar />;
+  return (
+    <SearchBar
+      hasError={hasError}
+      onRandomButtonClicked={randomPokemon}
+      onSearchButtonClicked={searchPokemon}
+    />
+  );
 }
