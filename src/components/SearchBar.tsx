1ec5093b14d962779a506760821591cddbdd6344
eXiga <anton.kostenich@hotmail.com>
2022-05-29 17:53:56 +0200

    Apply DaisyUI styling on SearchBar component

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -21,9 +21,9 @@ export default function SearchBar({
     <div className="flex flex-col justify-center items-center h-screen gap-y-5">
       <div>
         <input
-          className={`w-96 appearance-none border-2 border-solid rounded py-2 px-2 ${
-            hasError ? "border-red-700 outline-red-700" : ""
-          } text-sky-600 leading-tight italic placeholder:italic`}
+          className={`input ${
+            hasError ? "input-error" : "input-success"
+          } input-bordered w-96 leading-tight italic placeholder:italic`}
           id="name"
           type="text"
           placeholder="Pokemon name"
@@ -39,14 +39,14 @@ export default function SearchBar({
       </div>
       <div className="flex flex-row justify-start items-start gap-x-2">
         <button
-          className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          className="btn btn-primary"
           type="button"
           onClick={() => onSearchButtonClicked(name)}
         >
           Search
         </button>
         <button
-          className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          className="btn btn-accent"
           type="button"
           onClick={onRandomButtonClicked}
         >
